7360c8b15c3a92909342f01d48b7975c033214f7
Nicola <nicola.sebastianelli@studio.unibo.it>
2018-11-07 18:17:02 +0100

    Add files via upload

--- Code/BBA0Controller.py
+++ Code/BBA0Controller.py
@@ -16,7 +16,6 @@ from BaseController import BaseController
 
 DEBUG = 1
 
-
 # This controller is an implementation of the BBA0 Controller
 # described in Chapter 4 of the paper:
 # Zhi Li, et al, "A Buffer-Based Approach to Rate Adaptation: Evidence from a Large Video Streaming Service", Te-Yuan Huang, Ramesh Johari, Nick McKeown, Matthew Trunnell, Mark Watson Stanford University, Netflix
@@ -25,41 +24,42 @@ class BBA0Controller(BaseController):
 
     def __init__(self):
         super(BBA0Controller, self).__init__()
-        self.conf = {
-            "r": 90,
-            "cu": 126
-        }
 
     def __repr__(self):
         return '<BBA0Controller-%d>' % id(self)
 
-    def f(Buf_now):
+    def f(self, B_now):
         # The function f corresponds to the line equation between the points
         # (r,R_min) and (r+cu,R_max) when the value of Buf_now is
         # bounded by r and r+cu
-        return Buf_now * ((R_max - R_min) / self.conf["cu"]) + (
-                    R_min - ((self.conf["r"] / self.conf["cu"]) * (R_max - R_min)))
+        reservoir = self.feedback['max_buffer_time'] * 0.2
+        cushion = self.feedback['max_buffer_time'] * 0.8 - reservoir
+        R_max = self.feedback['max_rate']
+        R_min = self.feedback['min_rate']
+
+        return B_now * ((R_max - R_min) / cushion) + (R_min - ((reservoir / cushion) * (R_max - R_min)))
 
     def maxR(self, constraint):
         Rates = self.feedback['rates']
         result = Rates[0]
         for i in Rates:
-            if (i > constraint and result > i):
-                result = Rates[i]
+            if (i < constraint and result > i):
+                result = i
         return result
 
     def minR(self, constraint):
         Rates = self.feedback['rates']
         result = Rates[0]
         for i in Rates:
-            if (i < constraint and result < i):
+            if (i > constraint and result < i):
                 result = i
         return result
 
     def calcControlAction(self):
-        self.setIdleDuration(0.0)
 
         # Retrive current iteration variables
+        reservoir = self.feedback['max_buffer_time'] * 0.2
+        cushion = self.feedback['max_buffer_time'] * 0.8 - reservoir
         R_max = self.feedback['max_rate']
         R_min = self.feedback['min_rate']
         R_curr = self.feedback['cur_rate']
@@ -80,18 +80,18 @@ class BBA0Controller(BaseController):
         # Compute new rate based in current buffer region
 
         # Buffer in reservoir area
-        if B_now <= self.conf["r"]:
+        if B_now <= reservoir:
             Rate_next = R_min
 
         # Buffer in upper reservoir area
-        elif B_now >= self.conf["r"] + self.conf["cu"]:
+        elif B_now >= reservoir + cushion:
             Rate_next = R_max
 
         # Buffer in cushion area
-        elif f(Buf_now) >= R_plus:
-            Rate_next = self.maxR(f(Buf_now))
-        elif f(Buf_now) <= R_minus:
-            Rate_next = self.minR(f(Buf_now))
+        elif self.f(B_now) >= R_plus:
+            Rate_next = self.maxR(self.f(B_now))
+        elif self.f(B_now) <= R_minus:
+            Rate_next = self.minR(self.f(B_now))
 
         else:
             Rate_next = R_curr
